0bc1f738117a6b1d7ce36403c58187eacee59b3c
LNGiaHung <146319507+LNGiaHung@users.noreply.github.com>
2025-11-26 22:35:22 +0700

    Delete ABSA directory

--- ABSA/README.md
+++
@@ -1,3 +0,0 @@
-# ChauandThang
-Run prepare_vocab.py with script in build_vocab.py
-Run corenlp_poreprocess_data.py
--- ABSA/build_vocab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# build vocab for different datasets
-
-python3 ./prepare_vocab.py --data_dir dataset/Restaurants_corenlp --vocab_dir dataset/Restaurants_corenlp
-python3 ./prepare_vocab.py --data_dir dataset/Laptops_corenlp --vocab_dir dataset/Laptops_corenlp
